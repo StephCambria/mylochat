50184bcbb5725159ebdc4ae8bee851a70dd61bf2
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-12-15 20:05:47 -0500

    chat box and input rendering
    Signed-off-by: Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>

--- client/src/Context/chatProvider.js
+++ client/src/Context/chatProvider.js
@@ -6,8 +6,8 @@ const ChatContext = createContext();
 
 const ChatProvider = ({ children }) => {
   const [user, setUser] = useState();
-  const [selectedChat, setSelectedChat] = useState();
-  const [chats, setChats] = useState();
+  const [selectedChat, setSelectedChat] = useState([]);
+  const [chats, setChats] = useState([]);
   const history = useHistory();
 
   useEffect(() => {
